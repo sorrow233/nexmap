e8f375555372503ec88c6c54c94827ffaf07e731
sorrow233 <sorrow233@proton.me>
2025-01-22 20:43:30 +0900

    Auto-deploy and sync

--- src/components/BoardGallery.jsx
+++ src/components/BoardGallery.jsx
@@ -237,7 +237,7 @@ export default function BoardGallery({ boards, onSelectBoard, onCreateBoard, onD
             </div>
 
             {/* Grid */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pb-40">
                 {boards.map((board, index) => (
                     <div
                         key={board.id}
